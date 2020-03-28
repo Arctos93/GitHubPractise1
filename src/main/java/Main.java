3137b210c748e925f0673a7afd987814431d9752
Arkadiusz Żurek <Zureka@gazeta.pl>
2020-03-28 11:05:11 +0100

    Add exercise

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        List<Integer> listaLiczbCalkowitych = getListaLiczbCalkowitych();
+        System.out.println(getMinimum(listaLiczbCalkowitych));
+    }
+
+    public static int getMinimum(List<Integer> array) {
+        // Znajduje minimum
+        return 0;
+    }
+
+    public static List<Integer> getListaLiczbCalkowitych() throws IOException {
+        // Tworzy i inicjalizuje listę
+        return null;
+    }
+}
