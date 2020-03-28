f73f9280014257f8288da75319c85067d42bcfd5
kamill.masllanka <kamill.masllanka@gmail.com>
2020-03-28 12:55:20 +0100

    KamilVersion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -28,6 +28,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         List<Integer> listaLiczbCalkowitych = getListaLiczbCalkowitych();
         System.out.println(getMinimum(listaLiczbCalkowitych));
+
+        getListaLiczbCalkowitych();
+        getMinimum(listaLiczbCalkowitych);
+
     }
 
 
