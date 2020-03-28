e5cce8fb26e2e371c077213ecb0853f82f45a34b
kamill.masllanka <kamill.masllanka@gmail.com>
2020-03-28 13:48:40 +0100

    KamilVersion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,41 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Main {
+
     public static void main(String[] args) throws Exception {
         List<Integer> listaLiczbCalkowitych = getListaLiczbCalkowitych();
         System.out.println(getMinimum(listaLiczbCalkowitych));
+
+        getMinimum(listaLiczbCalkowitych);
+
     }
 
-    public static int getMinimum(List<Integer> array) {
-        Collections.sort(array);
-        return array.get(0);
-    }
+        public static int getMinimum (List < Integer > array) {
 
-    public static List<Integer> getListaLiczbCalkowitych() throws IOException {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt() ;
-        for (int i = 0; i <N ; i++) {
-        list.add(scanner.nextInt());
+            return array.stream().min(Integer::compareTo).get();
         }
-        return list;
-    }
+
+        public static List<Integer> getListaLiczbCalkowitych () throws IOException {
+            ArrayList<Integer> list = new ArrayList<>();
+
+            Scanner size = new Scanner(System.in);
+            System.out.println("Podaj ilość liczb całkowitych: ");
+            Integer s = size.nextInt();
+
+            for(int i = 0; i < s; i++){
+                list.add(size.nextInt());
+            } return list;
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Podaj liczbę całkowitą: ");
+//            list.stream()
+//                    .forEach(number -> list.add(number));
+//            return list;
+
+        }
 }
